2ded3cf4a5b438deb78e78fb02e63e7f426585e7
Uedson Reis <uedson.reis@sovos.com>
2022-09-15 08:17:01 -0300

    WIP

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages, setup
 
+# python setup.py bdist_wheel || python -m build
+# python -m twine upload --repository pypi dist/*
 
 setup(
     name='lib_ms_api',
     packages=find_packages(include=['lib_ms_api']),
     version='1.0.0',
     description='A Python library to build simple Microservices',
+    author_email='uedsonreis@gmail.com',
     author='Uedson Reis',
     license='ISC',
 )
